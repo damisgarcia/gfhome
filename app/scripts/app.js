96f52ac3cb2862a09d1a94bb5ce61d61e621bd98
Damis Garcia <damisgarcia@gmail.com>
2015-12-23 23:54:59 -0300

    Rebuild

---
+++ app/scripts/app.js
@@ -0,0 +1,62 @@
+angular.module('girafalesHomeApp',['ngAnimate','ui.bootstrap']).run(function($rootScope){
+  // Resize 
+
+  var scrollY = 0
+  $(window).scroll(function(e){
+    e.preventDefault()
+    if(scrollY < e.currentTarget.scrollY){
+      $rootScope.$topNav = false
+      scrollY = e.currentTarget.scrollY
+    }
+    else if(scrollY > (e.currentTarget.scrollY + 75) ){
+      $rootScope.$topNav = true
+      scrollY = e.currentTarget.scrollY
+    }
+    $rootScope.$apply()
+  });
+
+  // jQuery for page scrolling feature - requires jQuery Easing plugin
+  $(function() {
+    $('a.page-scroll').bind('click', function(event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+          scrollTop: $($anchor.attr('href')).offset().top - 75
+      }, 1500, 'easeInOutExpo');
+      event.preventDefault();
+    });
+  });
+
+  // Highlight the top nav as scrolling occurs
+  $('body').scrollspy({
+    target: '#top'
+  })
+
+  // Closes the Responsive Menu on Menu Item Click
+  $('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+  });
+
+  $('div.modal').on('show.bs.modal', function() {
+    var modal = this;
+    var hash = modal.id;
+    window.location.hash = hash;
+    window.onhashchange = function() {
+      if (!location.hash){
+        $(modal).modal('hide');
+      }
+    }
+  });
+
+  // console.log($("#services").offset().top)
+
+  $("#top").affix({
+    offset:{
+      top: $("#services").offset().top
+    }
+  })
+
+  // stops video on closing modal
+  $("#presentation-modal").on('hidden.bs.modal', function (e) {
+    $("#presentation-modal iframe").attr("src", $("#presentation-modal iframe").attr("src"));
+  });
+})
